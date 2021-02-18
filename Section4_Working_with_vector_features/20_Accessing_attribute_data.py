7a66d596d5eeec3578f3894b8611be4418fdd494
Darko Nedic <darko.nedic83@gmail.com>
2021-02-18 16:23:56 -0500

    Attribute data

---
+++ Section4_Working_with_vector_features/20_Accessing_attribute_data.py
@@ -0,0 +1,29 @@
+import time
+tmStart = time.time()
+
+layers = QgsProject.instance().mapLayersByName("Countries")
+if len(layers)>0:
+    countries = layers[0]
+    if countries.isValid():
+        sStr = ""
+        request = QgsFeatureRequest()
+        request.setFilterExpression("\"POP_EST\">100000")
+        for ftr in countries.getFeatures(request):
+#            if ftr['POP_EST']>100000:
+                if ftr['POP_EST']>100000000:
+                    pop_cat = "large"
+                elif ftr['POP_EST']<10000000:
+                    pop_cat = "small"
+                else:
+                    pop_cat = "moderate"
+                    
+                print("{} has a {} population".format(ftr['NAME'], pop_cat))
+                QgsMessageLog.logMessage("{} has a {} population".format(ftr['NAME'], pop_cat))
+        print(sStr)
+    else:
+        print("Countries is not a valid layer")
+else:
+    print("Countries not found")
+
+tmEnd = time.time()
+print("Run time = {0:.3f} seconds".format(tmEnd-tmStart))
